15821ba06e5b36893e3c3e77a689f30e25b2f185
khushi <khusbumandal2060@gmail.com>
2025-01-09 12:37:06 +0545

    binary search

---
+++ BinarySearch.java
@@ -0,0 +1,54 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int nums[] = {5,7,9,11,13};
+        int target = 16;
+
+        int result = binarySearch(nums, target);
+
+        if(result != -1)
+          System.out.println("Element found at Index: " + result);
+        else
+         System.out.println("Element not found");
+    }
+
+
+
+    // LinearS earch
+    public static int linearSearch(int[] nums, int target) {
+        int steps = 0;
+        for(int i=0; i<nums.length; i++){
+            steps++;
+            if(nums[i] == target)
+            return i;
+        }
+    
+    System.out.println("steps taken by linear: " + steps);
+        return -1;
+    }
+
+
+//Binary search
+public static int binarySearch(int[] nums, int target) {
+    //7,7,9,11,13
+    int steps = 0;
+    int left = 0;
+    int right = nums.length-1;
+
+    while(left <= right) {
+        steps++;
+        int mid = (left + right)/2;
+
+        if(nums[mid] == target) {
+            System.out.println("Steps taken by Binary: " +steps);
+            return mid;
+        }
+        else if(nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    System.out.println("Steps taken by Binary: " +steps);
+     return -1;
+}
+}
